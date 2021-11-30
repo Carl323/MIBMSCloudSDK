bfe651bad2fb20925d5567a95762b4eacdb23fd4
PluviophileS <muran323@163.com>
2021-11-30 21:34:12 +0800

    删除了没有被开发和使用的FaceRecognitionServer类。

--- MIBMSCloudSDK/private/FaceRecognitionServer.cpp
+++
@@ -1,8 +0,0 @@
-#include "..\public\FaceRecognitionServer.h"
-FaceRecognitionServer::FaceRecognitionServer()
-{
-}
-
-FaceRecognitionServer::~FaceRecognitionServer()
-{
-}
--- MIBMSCloudSDK/public/FaceRecognitionServer.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "MIBMSCloud.h"
-
-class FaceRecognitionServer :public server
-{
-public:
-	FaceRecognitionServer();
-	~FaceRecognitionServer();
-};
